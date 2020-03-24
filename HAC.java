45fff890698cafe2667b11d5c41c1ab149bc46e4
ds6546 <55463801+ds6546@users.noreply.github.com>
2020-03-23 19:22:01 -0500

    Add files via upload

--- HAC.java
+++ HAC.java
@@ -15,7 +15,7 @@ public class HAC {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		List<Client> net = Collections.synchronizedList(new ArrayList<Client>());
+		
 		System.out.println("Are you a client or a server? \n"
                 + "Enter \"C\" if you are a client and \"S\" if you are a server");
         Scanner keyboard = new Scanner(System.in);
@@ -23,26 +23,33 @@ public class HAC {
         
         if(input == 'C')
         {
+            List<Client> nettoserver = new ArrayList<>();
             System.out.println("Hey client, what is the server IP address you want to connect to?");
             Scanner k2 = new Scanner(System.in);
             String server_address = k2.nextLine();
             try{ 
             	InetAddress add = InetAddress.getByName(server_address); 
-            	UDPClientD c = new UDPClientD(add, net);
-            	c.executeClient();
+            	UDPClientD c = new UDPClientD(add);
+            	nettoserver = c.executeClient();
+                
+                System.out.println("Printing out the arraylist in the HAC");
+               /* for(int i = 0; i< nettoserver.size(); i++)
+                {
+                    nettoserver.get(i).printIP();
+                }*/
             }
             catch(UnknownHostException e)
             {
                 System.out.println("Server not found!");
             }
-            UDPServerD theServer = new UDPServerD(net);
+            UDPServerD theServer = new UDPServerD(nettoserver);
             theServer.executeServer();
         }
         
         else if(input == 'S')
         {
             System.out.println("I am the server, I am receiving the data");
-            UDPServerD theServer = new UDPServerD(net);
+            UDPServerD theServer = new UDPServerD();
             theServer.executeServer();
         }
         keyboard.close();
